e2220c6f35c64ce9ba523e3bf7fb0ce75b91fb53
Brian Anderson <banderson@mozilla.com>
2015-01-14 10:21:40 -0800

    Don't get the cargo rev from the source tarball

--- fetch-inputs.py
+++ fetch-inputs.py
@@ -54,22 +54,27 @@ os.chdir(cwd)
 
 # Get list of rust artifacts for target
 rust_artifacts = []
+rustc_installer = None
 for line in open(os.path.join(TEMP_DIR, rust_manifest_name)):
     if target in line and ".tar.gz" in line:
         rust_artifacts.append(line.rstrip())
+        if line.startswith("rustc-") and "-src" not in line:
+            rustc_installer = line.rstrip()
 assert len(rust_artifacts) > 0
 print "rust artifacts: " + str(rust_artifacts)
 
-# Get the source artifact from which we'll dig out the version number
-# in order to figure out which cargo to pair with for non-nightly channels.
-# We'll also use the source checksum as a fingerprint for synchronizing
+assert rustc_installer is not None
+
+# We'll use the source checksum as a fingerprint for synchronizing
 # dist builds across platforms on the buildbot prior to uploading.
+# FIXME: Would be nice to get this fingerprint from the 'version' file
+# but that requires some buildbot changes.
 rust_source = None
 for line in open(os.path.join(TEMP_DIR, rust_manifest_name)):
     if "-src" in line:
         rust_source = line.rstrip()
-assert rust_source is not None
-print "rust source: " + rust_source
+        assert rust_source is not None
+        print "rust source: " + rust_source
 
 # Download the source
 cwd = os.getcwd()
@@ -81,25 +86,36 @@ if retval != 0:
     sys.exit(1)
 os.chdir(cwd)
 
+# Download the rust artifacts
+full_rust_artifacts = [remote_rust_dir + "/" + x for x in rust_artifacts]
+for artifact in full_rust_artifacts:
+    cwd = os.getcwd()
+    os.chdir(IN_DIR)
+    retval = subprocess.call(["curl", "-f", "-O", artifact])
+    if retval != 0:
+        print "downlading " + artifact + " failed"
+        sys.exit(1)
+    os.chdir(cwd)
+
+
 # Figure out corresponding cargo nightly. If the channel is nightly, then it's just the cargo nightly.
 # If it's beta or stable then it's paired with a specific revision from the cargo-snap.txt file.
 cargo_archive_date = None
 if channel != "nightly":
-    retval = subprocess.call(["tar", "xzf", IN_DIR + "/" + rust_source, "-C", TEMP_DIR])
+    retval = subprocess.call(["tar", "xzf", IN_DIR + "/" + rustc_installer, "-C", TEMP_DIR])
     if retval != 0:
         print "untarring source failed"
         sys.exit(1)
-    rust_source_dir = os.path.join(TEMP_DIR, rust_source.replace("-src.tar.gz", ""))
+    rustc_installer_dir = os.path.join(TEMP_DIR, rustc_installer.replace(".tar.gz", ""))
 
-    # Pull the version number *out of the makefiles*. Seriously.
+    # Pull the version number out of the version file
     version = None
-    for line in open(os.path.join(rust_source_dir, "mk", "main.mk")):
-        if "CFG_RELEASE_NUM" in line:
-            version = line.split("=")[1].strip()
-            assert len(version) > 0
-            break
+    for line in open(os.path.join(rustc_installer_dir, "version")):
+        print "reported version: " + line
+        version = line.split(" ")[0].split("-")[0]
 
     assert version is not None
+    print "cargo version key: " + version
 
     # Search the cargo snap database for this version
     for line in open("cargo-revs.txt"):
@@ -137,12 +153,9 @@ for line in open(os.path.join(TEMP_DIR, cargo_manifest_name)):
 assert len(cargo_artifacts) > 0
 print "cargo artifacts: " + str(cargo_artifacts)
 
-# Download all the artifacts
-
-full_rust_artifacts = [remote_rust_dir + "/" + x for x in rust_artifacts]
+# Download the cargo artifacts
 full_cargo_artifacts = [remote_cargo_dir + "/" + x for x in cargo_artifacts]
-all_artifacts = full_rust_artifacts + full_cargo_artifacts + [full_rust_source]
-for artifact in all_artifacts:
+for artifact in full_cargo_artifacts:
     cwd = os.getcwd()
     os.chdir(IN_DIR)
     retval = subprocess.call(["curl", "-f", "-O", artifact])
